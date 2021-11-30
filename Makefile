a7ad2858075d9ad7a446b7fd97ee3631fa4902ee
Pablo Halpern <phalpern@halpernwightsoftware.com>
2021-11-30 16:46:49 -0500

    checkpoint

--- Makefile
+++ Makefile
@@ -3,30 +3,35 @@
 
 CXX = g++
 OPT = -O3
-# OPT = -g
 OBJ = obj
-CXXFLAGS = -std=c++17 $(OPT) $(STDLIB)
+CXXFLAGS = -std=c++17 -Wall $(STDLIB)
 
 XTRA_ARGS ?=
 
 RESULTS = results
-BINARIES = $(OBJ)/benchmark
+BINARIES     = $(OBJ)/benchmark
+DBG_BINARIES = $(OBJ)/dbg-benchmark
 
 MKDIRS := $(shell mkdir -p $(OBJ) $(RESULTS))
 
 all : $(BINARIES)
 
-smoke : all
-	$(OBJ)/benchmark 4 2 3 1 1 5 -v -p
+debug : $(DBG_BINARIES)
+
+smoke : $(BINARIES)
+	$(OBJ)/benchmark . 2^4 16 8 1 2^0 1k -vp
 
 $(OBJ)/% : %.cpp
-	$(CXX) $(CXXFLAGS) -o $@ $<
+	$(CXX) $(CXXFLAGS) $(OPT) -o $@ $<
+
+$(OBJ)/dbg-% : %.cpp
+	$(CXX) $(CXXFLAGS) -g -o $@ $<
 
 test.%: $(BINARIES)
 	./runtest $(XTRA_ARGS) $*
 
 clean:
-	rm -f $(BINARIES)
+	rm -f $(BINARIES) $(DBG_BINARIES)
 
 cleanall:
 	rm -rf $(OBJ) $(RESULTS)
--- benchmark.cpp
+++ benchmark.cpp
@@ -13,6 +13,10 @@
 
 namespace chrono = std::chrono;
 
+constexpr unsigned KiB = 1024;
+constexpr unsigned MiB = 1024 * KiB;
+constexpr unsigned GiB = 1024 * MiB;
+
 using Element   = std::pmr::vector<char>;
 using Subsystem = std::pmr::vector<Element>;
 using System    = std::pmr::vector<Subsystem>;
@@ -20,19 +24,19 @@ using System    = std::pmr::vector<Subsystem>;
 bool verbose      = false;
 bool showProgress = false;
 
-std::ptrdiff_t systemSize    ;
-std::ptrdiff_t numSubsystems ;
-std::ptrdiff_t elemsPerSubsys;
-std::ptrdiff_t elemSize      ;
-std::ptrdiff_t iterationCount;
-std::ptrdiff_t churnCount    ;
-std::ptrdiff_t repCount      ;
+std::size_t systemSize    ;
+std::size_t numSubsystems ;
+std::size_t elemsPerSubsys;
+std::size_t elemSize      ;
+std::size_t churnCount    ;
+std::size_t accessCount   ;
+std::size_t repCount      ;
 
 void initializeSubsystem(Subsystem* ss)
   // Initialize `ss` to `elemsPerSubsys` elements of `elemSize` length.
 {
     ss->reserve(elemsPerSubsys);
-    for (std::ptrdiff_t i = 0; i < elemsPerSubsys; ++i) {
+    for (std::size_t i = 0; i < elemsPerSubsys; ++i) {
         char c = 'A' + (std::rand() & 31);
         Element& e = ss->emplace_back();
         e.reserve(elemSize);
@@ -41,13 +45,13 @@ void initializeSubsystem(Subsystem* ss)
     }
 }
 
-void accessSubsystem(Subsystem* ss, std::ptrdiff_t iterationCount)
+void accessSubsystem(Subsystem* ss, std::size_t accessCount)
     // Ping the subsystem, simulating read/write accesses proportional to the
-    // specified `iterationCount`.
+    // specified `accessCount`.
 {
     if (ss->empty()) return;  // nothing to access
 
-    for (std::ptrdiff_t i = 0; i < iterationCount; ++i) {
+    for (std::size_t i = 0; i < accessCount; ++i) {
         for (Element& e : *ss) {
             // XOR last 3 bits of each byte of element into first byte
             char x = 0;
@@ -79,19 +83,19 @@ struct CopyOrMove_t<false>
 };
 
 template <bool UseCopy>
-void churn(System *system, std::ptrdiff_t churnCount)
+void churn(System *system, std::size_t churnCount)
 {
-    using std::ptrdiff_t;
+    using std::size_t;
 
     static std::mt19937 rengine;
-    static std::uniform_int_distribution<ptrdiff_t> urand(0, system->size()-1);
+    static std::uniform_int_distribution<size_t> urand(0, system->size()-1);
 
-    const ptrdiff_t nS = system->size();
-    const ptrdiff_t sS = (*system)[0].size(); // Subsystem size
+    const size_t nS = system->size();
+    const size_t sS = (*system)[0].size(); // Subsystem size
 
     // Vector of indexes used to shuffle elements between subsystems
-    static std::vector<ptrdiff_t> randomSeq(nS);
-    for (ptrdiff_t i = 0; i < nS; ++i) {
+    static std::vector<size_t> randomSeq(nS);
+    for (size_t i = 0; i < nS; ++i) {
         randomSeq[i] = i;
     }
 
@@ -108,8 +112,8 @@ void churn(System *system, std::ptrdiff_t churnCount)
     Element tempElem(tempAlloc);
 
     // Repeat shuffle 'churnCount' times (default 1)
-    for (ptrdiff_t c = 0; c < churnCount; ++c) {
-        for (ptrdiff_t e = 0; e < sS; ++e) {
+    for (size_t c = 0; c < churnCount; ++c) {
+        for (size_t e = 0; e < sS; ++e) {
             // Rotate values randomly through 'nS' elements at rank 'e'
             std::shuffle(randomSeq.begin(), randomSeq.end(), rengine);
             Element *hole = &tempElem;
@@ -125,8 +129,8 @@ void churn(System *system, std::ptrdiff_t churnCount)
 }
 
 template <typename TP>
-void progress(const char* label, TP& snapShot, std::ptrdiff_t rep,
-              std::ptrdiff_t ss, const char* msg)
+void progress(const char* label, TP& snapShot, std::size_t rep,
+              std::size_t ss, const char* msg)
 {
     using namespace std::literals::chrono_literals;
 
@@ -166,11 +170,11 @@ chrono::milliseconds doTest(void* buffer, std::size_t totalBytes)
 
     auto startTime = chrono::steady_clock::now();
 
-    for (std::ptrdiff_t n = 0; n < repCount; ++n) {
+    for (std::size_t n = 0; n < repCount; ++n) {
         churn<UseCopy>(&system, churnCount);
         if (showProgress) progress(label, snapShot, n, 0, "churned");
-        for (long ss = 0; ss < numSubsystems; ++ss) {
-	    accessSubsystem(&system[ss], iterationCount);
+        for (std::size_t ss = 0; ss < numSubsystems; ++ss) {
+	    accessSubsystem(&system[ss], accessCount);
             if (showProgress)
                 progress(label, snapShot, n, ss, "accessed");
         }
@@ -203,19 +207,91 @@ void processOptions(const char* argv[], int argc, int& arg)
     } // /end while
 }
 
-std::ptrdiff_t parseArg(const char* argv[], int argc, int& arg,
-                        std::ptrdiff_t dflt)
+// Wrapper class for print formatting. `std::cout << PrintSize(x)` will print
+// `x` formatted as a decimal size abbreviated by using the 'G', 'M', or 'K'
+// suffix if possible.
+class PrintSize
+{
+    std::size_t d_value;
+
+public:
+    explicit PrintSize(std::size_t v) : d_value(v) { }
+
+    friend std::ostream& operator<<(std::ostream& os, PrintSize s) {
+        std::size_t v = s.d_value;
+        if ((v & (GiB - 1)) == 0)
+            return os << (v >> 30) << 'G';
+        else if ((v & (MiB - 1)) == 0)
+            return os << (v >> 20) << 'M';
+        else if ((v & (KiB - 1)) == 0)
+            return os << (v >> 10) << 'K';
+        else
+            return os << v;
+    }
+};
+
+// Parse a size expressed in the following format:
+//
+//     [2^] _integer_ [K|M|G]
+//
+// There are no whitespace allowed in this format.  The _integer_ portion is
+// any valid format parsable by `std::strtoull`; let _N_ be the value returned
+// from `std::strtoul("_integer_")`.  If the `2^` prefix is present, then the
+// computed value is 2^N.  If a 'K', 'M', or 'G' suffix is present, then the
+// resulting value is multipled by 2^10, 2^20, or 2^30, respectively; the
+// suffix is case-insensitive.  Note that the prefix is applied to _N_ before
+// the suffix.
+std::size_t parseSize(const char* str)
+{
+    bool error      = false;
+    bool isExponent = false;
+
+    const char* cursor = str;
+    if ('2' == cursor[0] && '^' == cursor[1]) {
+        isExponent = true;
+        cursor += 2;
+    }
+
+    char* endCursor = nullptr;
+    std::size_t result = std::strtoull(cursor, &endCursor, 0);
+    if (endCursor == cursor) error = true;
+    cursor = endCursor;
+
+    if (isExponent)
+        result = 1 << result;
+
+    switch (*cursor) {
+        case 'G':
+        case 'g': result *= GiB; ++cursor; break;
+
+        case 'M':
+        case 'm': result *= MiB; ++cursor; break;
+
+        case 'K':
+        case 'k': result *= KiB; ++cursor; break;
+    }
+
+    if (error || '\0' != *cursor) {
+        std::cerr << "Error: Bad size argument: " << str << std::endl;
+        std::exit(1);
+    }
+
+    return result;
+}
+
+constexpr size_t placeholderArg = std::numeric_limits<std::size_t>::max();
+
+std::size_t parseArg(const char* argv[], int argc, int& arg, std::size_t dflt)
     // Parse argument specified by `arg` into a number. If `argv[arg]` is
     // missing, then return the specified `dflt`. If `argv[arg]` specifies the
-    // placeholder, ".", then return -1. Otherwise, return `argv[arg]`
-    // converted to a positive integer. If `argv[arg]` represents a negative
-    // integer, an error is emited.
+    // placeholder, ".", then return `placeholderArg`. Otherwise, return
+    // `argv[arg]` interpreted as a size (see `parseSize`).
 {
     processOptions(argv, argc, arg);
     if (arg < argc) {
         int a = arg++;
-        if ('.' == argv[a][0]) return -1; // Placeholder
-        return atoi(argv[a]);
+        if ('.' == argv[a][0]) return placeholderArg; // Placeholder
+        return parseSize(argv[a]);
     }
     else
         return dflt;
@@ -229,81 +305,77 @@ std::ptrdiff_t parseArg(const char* argv[], int argc, int& arg,
 int main(int argc, const char *argv[])
 {
     int a = 1;
-    systemSize       = parseArg(argv, argc, a, 18);
-    numSubsystems    = parseArg(argv, argc, a, 4);
-    elemsPerSubsys   = parseArg(argv, argc, a, 20);
-    elemSize         = parseArg(argv, argc, a, 3);
-    iterationCount   = parseArg(argv, argc, a, 18);
-    churnCount       = parseArg(argv, argc, a, 1);
-    repCount         = parseArg(argv, argc, a, 3);
+    systemSize     = parseArg(argv, argc, a, 256*KiB);
+    numSubsystems  = parseArg(argv, argc, a, 16);
+    elemsPerSubsys = parseArg(argv, argc, a, 1*MiB);
+    elemSize       = parseArg(argv, argc, a, 8);
+    churnCount     = parseArg(argv, argc, a, 1);
+    accessCount    = parseArg(argv, argc, a, 8);
+    repCount       = parseArg(argv, argc, a, 4*KiB);
 
     processOptions(argv, argc, a);
 
-    if ((systemSize < 0) + (numSubsystems < 0) + (elemsPerSubsys < 0) +
-        (elemSize < 0) > 1) {
+    // Placeholder sizes are replaced by sizes computed from other arguments.
+    if ((systemSize == placeholderArg)     +
+        (numSubsystems == placeholderArg)  +
+        (elemsPerSubsys == placeholderArg) +
+        (elemSize == placeholderArg)       > 1) {
         std::cerr << "Error: Only one of systemSize, numSubsystems, "
             "elemsPerSubsys, or elemSize can be defaulted\n";
-        return -1;
+        return 1;
     }
 
-    // Arguments are all exponents of two; therefore, to compute their product,
-    // you ADD them (not multiply them).
-    if (systemSize < 0)
-        systemSize = numSubsystems + elemsPerSubsys + elemSize;
+    if (systemSize == placeholderArg)
+        systemSize = numSubsystems * elemsPerSubsys * elemSize;
 
-    if (numSubsystems < 0) {
-        numSubsystems = systemSize - elemsPerSubsys - elemSize;
-        if (numSubsystems < 0) {
+    if (numSubsystems == placeholderArg) {
+        numSubsystems = systemSize / (elemsPerSubsys * elemSize);
+        if (numSubsystems < 1) {
             std::cerr <<
-                "Error: systemSize must be >= elemsPerSubsys + elemSize\n";
-            return -1;
+                "Error: systemSize must be >= elemsPerSubsys * elemSize\n";
+            return 1;
         }
     }
 
-    if (elemsPerSubsys < 0) {
-        elemsPerSubsys = systemSize - numSubsystems - elemSize;
-        if (numSubsystems < 0) {
+    if (elemsPerSubsys == placeholderArg) {
+        elemsPerSubsys = systemSize / (numSubsystems * elemSize);
+        if (elemsPerSubsys < 1) {
             std::cerr <<
-                "Error: systemSize must be >= numSubsystems + elemSize\n";
-            return -1;
+                "Error: systemSize must be >= numSubsystems * elemSize\n";
+            return 1;
         }
     }
 
-    if (elemSize < 0) {
-        elemSize = systemSize - numSubsystems - elemsPerSubsys;
-        if (elemSize < 0) {
+    if (elemSize == placeholderArg) {
+        elemSize = systemSize / (numSubsystems * elemsPerSubsys);
+        if (elemSize < 1) {
             std::cerr <<
-                "Error: systemSize must be >= numSubsystems + elemsPerSubsys\n";
-            return -1;
+                "Error: systemSize must be >= numSubsystems * elemsPerSubsys\n";
+            return 1;
         }
     }
 
-    std::cout << systemSize     << ','
-              << numSubsystems  << ','
-              << elemsPerSubsys << ','
-              << elemSize       << ','
-              << iterationCount << ','
-              << churnCount     << ','
-              << repCount       << std::endl;
+    // Placeholder loop counts are defaulted
+    if (placeholderArg == churnCount ) churnCount  = 1;
+    if (placeholderArg == accessCount) accessCount = 8;
+    if (placeholderArg == repCount   ) repCount    = 4*KiB;
 
-    // Convert exponents of 2 to regular numbers.
-    // Defaulted values become zero.
-    systemSize     = 1 << systemSize;
-    numSubsystems  = 1 << numSubsystems;
-    elemsPerSubsys = 1 << elemsPerSubsys;
-    elemSize       = 1 << elemSize;
-    iterationCount = iterationCount < 0 ? 0 : 1 << iterationCount;
-    churnCount     = churnCount     < 0 ? 0 : 1 << churnCount;
-    repCount       = repCount       < 0 ? 1 : 1 << repCount;
+    std::cout << PrintSize(systemSize)     << ','
+              << PrintSize(numSubsystems)  << ','
+              << PrintSize(elemsPerSubsys) << ','
+              << PrintSize(elemSize)       << ','
+              << PrintSize(churnCount)     << ','
+              << PrintSize(accessCount)    << ','
+              << PrintSize(repCount)       << std::endl;
 
     if (verbose) {
-        std::cerr << "systemSize     = " << systemSize << '\n'
-                  << "numSubsystems  = " << numSubsystems << '\n'
-                  << "elemsPerSubsys = " << elemsPerSubsys << '\n'
-                  << "elementSize    = " << elemSize << '\n'
-                  << "churnCount     = " << churnCount << '\n'
-                  << "iterationCount = " << iterationCount << '\n'
-                  << "repCount       = " << repCount << '\n';
+        std::cerr << "systemSize     = " << PrintSize(systemSize)     << '\n'
+                  << "numSubsystems  = " << PrintSize(numSubsystems)  << '\n'
+                  << "elemsPerSubsys = " << PrintSize(elemsPerSubsys) << '\n'
+                  << "elementSize    = " << PrintSize(elemSize)       << '\n'
+                  << "churnCount     = " << PrintSize(churnCount)     << '\n'
+                  << "accessCount    = " << PrintSize(accessCount)    << '\n'
+                  << "repCount       = " << PrintSize(repCount)       << '\n';
     }
 
     static constexpr int cachelineSize = 64;
@@ -318,8 +390,8 @@ int main(int argc, const char *argv[])
     void* buffer = ::operator new(totalBytes);
 
     chrono::milliseconds copyMs = doTest<true>(buffer, totalBytes);
-    std::cout << copyMs.count() << std::endl;
-
     chrono::milliseconds moveMs = doTest<false>(buffer, totalBytes);
+
+    std::cout << copyMs.count() << std::endl;
     std::cout << moveMs.count() << std::endl;
 }
