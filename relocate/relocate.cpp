0cfa641c7ab5fb09d67710adafe7538a06735cbe
Pablo Halpern <phalpern@halpernwightsoftware.com>
2023-06-23 18:01:49 -0400

    Not quite working relocate library

--- relocate/relocate.cpp
+++ relocate/relocate.cpp
@@ -3,85 +3,122 @@
 #include <type_traits>
 #include <cassert>
 
-template <class T> class DestructiveRefImp;
+namespace xstd {
+
+template <class T> class Relocator;
+template <class T> class MoveRelocator;
+template <class T> class TrivialRelocator;
+
+template <class T>
+struct is_explicitly_relocatable : std::is_convertible<Relocator<T>, T> {};
+
+template <class T>
+inline constexpr bool is_explicitly_relocatable_v =
+    is_explicitly_relocatable<T>::value;
+
+template <class T>
+struct is_trivially_relocatable
+    : std::bool_constant<std::is_trivially_move_constructible_v<T> ||
+                         std::is_convertible_v<TrivialRelocator<T>, T>>
+{};
+
+template <class T>
+inline constexpr bool is_trivially_relocatable_v =
+    is_trivially_relocatable<T>::value;
+
+template <class T>
+requires is_explicitly_relocatable_v<T>
+inline constexpr
+Relocator<T> relocate(T& v) noexcept { return Relocator<T>(v); }
+
+template <class T>
+requires std::is_trivially_move_constructible_v<T>
+inline constexpr T&& relocate(T& v) noexcept { return std::move(v); }
+
+template <class T>
+inline constexpr
+MoveRelocator<T> relocate(T& v) noexcept { return MoveRelocator<T>(v); }
 
 enum class RelocateState { engaged, exploded, released };
 
 template <class T>
-class DestructiveRefImp
+class Relocator
 {
     T&            m_source;
     RelocateState m_state;
 
   public:
-    explicit DestructiveRefImp(T& r) noexcept
+    constexpr explicit Relocator(T& r) noexcept
         : m_source(r), m_state(RelocateState::engaged) { }
 
-    DestructiveRefImp(const DestructiveRefImp&& dr) = delete; // move-only
+    Relocator(const Relocator&& dr) = delete; // move-only
 
-    DestructiveRefImp(DestructiveRefImp&& dr) noexcept // move-only
+    Relocator(Relocator&& dr) noexcept // move-only
         : m_source(dr.source)
         , m_state(dr.m_state)
     {
         dr.m_state = RelocateState::released;
     }
 
-    ~DestructiveRefImp()
+    constexpr ~Relocator()
         { if (m_state == RelocateState::engaged) m_source.~T(); }
 
-    DestructiveRefImp& operator=(const DestructiveRefImp&) = delete;
-    DestructiveRefImp& operator=(DestructiveRefImp&&) = delete;
+    Relocator& operator=(const Relocator&) = delete;
+    Relocator& operator=(Relocator&&) = delete;
 
-    operator T&&() && noexcept { return std::move(release(*this)); }
+    constexpr RelocateState state() const { return m_state; }
+    constexpr T& explode()
+    {
+        assert(m_state != RelocateState::released);
+        m_state = RelocateState::exploded;
+        return m_source;
+    }
+
 
     // HIDDEN FRIENDS
-    friend T& get(const DestructiveRefImp& dr)
+    friend T& reloc_get_imp(const Relocator& dr)
     {
         assert(dr.m_state != RelocateState::released);
         return dr.m_source;
     }
 
-    friend T& release(DestructiveRefImp& dr)
+    friend T& release(Relocator& dr)
     {
         assert(dr.m_state != RelocateState::exploded);
         dr.m_state = RelocateState::released;
         return dr.m_source;
     }
 
-    friend T& release(DestructiveRefImp&& dr) { return release(dr); }
-
-    template <class M>
-    friend auto explode(DestructiveRefImp& v, M T::*mp) noexcept
-    {
-        assert(v.m_state != RelocateState::released);
-        v.m_state = RelocateState::exploded;
-        return relocate(get(v).*mp);
-    }
+    friend T& release(Relocator&& dr) { return release(dr); }
 };
 
 template <class T>
-using DestructiveRef = std::conditional_t<std::is_trivially_copyable_v<T>,
-                                          const T&, DestructiveRefImp<T> >;
-
-template <class T>
-requires std::is_trivially_copyable_v<T>
-inline const T& get(const T& r) { return r; }
-
-template <class T>
-requires std::is_trivially_copyable_v<T>
-inline const T& release(const T& r) { return r; }
-
-template <class T>
-inline DestructiveRef<T> relocate(T& v) noexcept {return DestructiveRef<T>(v);}
-
-template <class T>
-inline DestructiveRefImp<T> relocate(DestructiveRefImp<T> v) noexcept
+struct MoveRelocator : Relocator<T>
 {
-    return std::move(v);
-}
+    using Relocator<T>::Relocator;
 
-#define RELOC_EXPLODE(DR, MEMB_NAME) \
-    explode(DR, &std::decay_t<decltype(get(DR))>::MEMB_NAME)
+    operator T&&() const { return std::move(reloc_get_imp(*this)); }
+
+    friend T& reloc_get_imp(const MoveRelocator& dr)
+        { return reloc_get_imp(static_cast<const Relocator<T>&>(dr)); }
+    friend T& release(MoveRelocator& dr)
+        { return release(static_cast<Relocator<T>&>(dr)); }
+};
+
+template <class T>
+requires (! std::is_trivially_move_constructible_v<T>)
+inline constexpr const T& reloc_get_imp(const T& r) { return r; }
+
+template <class T>
+inline constexpr auto reloc_get(const T& r) { return reloc_get_imp(r); }
+
+template <class T>
+requires (! is_explicitly_relocatable_v<T>)
+inline constexpr const T& release(const T& r) { return r; }
+
+#define RELOC_EXPLODE(R, MEMB_NAME) xstd::relocate(R.explode().MEMB_NAME)
+
+}  // Close namespace xstd.
 
 // Class with no relocating constructor
 class W
@@ -97,7 +134,18 @@ class W
                   << " and data = " << m_data << std::endl;
     }
 
-    W(const W& other) : m_self(this), m_data(other.m_data) { /* ... */ }
+    W(const W& other) : m_self(this), m_data(other.m_data)
+    {
+        std::cout << "Copy constructing W with this = " << this
+                  << " and data = " << m_data << std::endl;
+    }
+
+    W(W&& other) : m_self(this), m_data(other.m_data)
+    {
+        other.m_data = 0;
+        std::cout << "Move constructing W with this = " << this
+                  << " and data = " << m_data << std::endl;
+    }
 
     ~W()
     {
@@ -139,7 +187,7 @@ class X
                   << m_data2.value() << ')' << std::endl;
     }
 
-    X(DestructiveRefImp<X>&& other)
+    X(xstd::Relocator<X> other)
         : m_data1(RELOC_EXPLODE(other, m_data1))
         , m_data2(RELOC_EXPLODE(other, m_data2))
     {
@@ -159,21 +207,30 @@ class X
 int main()
 {
     // Test scenarios:
+
+    // Trivially moveable types
     {
         int x = 5;
-        int y = relocate(x);
+        int y = xstd::relocate(x);
         std::cout << "y = " << y << std::endl;
+
+        struct Aggregate { int first; double second; };
+        Aggregate p1{ 8, 9.2 };
+        Aggregate p2 = xstd::relocate(p1);
+        std::cout << "p2 = (" << p2.first << ", " << p2.second << ")\n";
     }
 
+    // Movable type
     {
         W *pw1 = new W(1);
-        W w2 = relocate(*pw1);
+        W w2 = xstd::relocate(*pw1);
         ::operator delete(pw1);
     }
 
+    // Explicitly relocatable type
     {
         X *px1 = new X(2, 3);
-        X x2 = relocate(*px1);
+        X x2 = xstd::relocate(*px1);
         ::operator delete(px1);
     }
 }
