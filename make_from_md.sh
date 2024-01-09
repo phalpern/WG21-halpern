7ae54d4de06495f9214f6797c5675e33e9c21300
Pablo Halpern <phalpern@halpernwightsoftware.com>
2024-01-09 13:36:45 -0500

    Commit pre-Kona P3002

--- make_from_md.sh
+++ make_from_md.sh
@@ -1,18 +1,24 @@
 #! /bin/bash
 
 if [ $# -ne 1 ]; then
-    echo >&2 Usage: "make_from_md [ file.pdf | file.html ]"
+    echo >&2 Usage: "make_from_md [ file.pdf | file.html | file.pdf.show | file.html.show ]"
     exit 1
 fi
 
+pdfreader="/mnt/c/Program Files (x86)/Foxit Software/Foxit PDF Reader/FoxitPDFReader.exe"
+
+set -x
+
 base=$(basename ${1%%.*})
-dest_ext=${1##*.}
+dest_ext=${1#*.}
 src=$(ls $base*.md)
 
 paper=$(sed -n -e '/^% [PD][0-9][0-9][0-9][0-9][Rr][0-9]\+\b/s/^% \([PD][0-9][0-9][0-9][0-9][Rr][0-9]\+\)\b.*$/\1/p' $src)
 dest=$paper.$dest_ext
 
 cat > tmp.mk <<EOF
+
+pdfreader="$pdfreader"
 
 $paper.pdf : $src
 	pandoc -s -V geometry:margin=1in -V colorlinks=true --number-sections -f markdown \$< -o \$@
@@ -23,6 +29,11 @@ $paper.html : $src
 	sed -e '/^%/d' < \$< >> $paper.htmlsrc.md
 	pandoc -s -f markdown -t html $paper.htmlsrc.md -o \$@
 	rm $paper.htmlsrc.md
+
+$paper.pdf.show : $paper.pdf .FORCE
+	"$pdfreader" "L:/$PWD/$<"
+
+.FORCE:
 
 EOF
 
